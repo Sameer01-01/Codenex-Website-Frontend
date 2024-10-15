d2b11ce954731aae41d6588e4e2dd104c160670c
Sameer Yadav <ysameer0026@gmail.com>
2024-10-16 02:20:07 +0530

    completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,52 +1,71 @@
-import f1 from '../assets/f1.png'
-import f2 from '../assets/f2.png'
-import { GrInstagram } from "react-icons/gr";
-import { FaTwitter } from "react-icons/fa";
-import { FaLinkedinIn } from "react-icons/fa";
+import { FaTwitter, FaInstagram, FaLinkedinIn } from 'react-icons/fa';
 import { IoLogoWhatsapp } from "react-icons/io5";
-
+import ftr from '../assets/ftr.png'
 
 const Footer = () => {
     return (
         <>
-            <hr />
-            <footer className="bg-black flex">
-                <div>
-
-                    <div className="pt-6 pl-5">
-                        <img src={f1} alt="" className='absolute w-[70px] ' />
-                        <p className='text-white text-xl mt-3 w-[300px] ml-20'>Codenex is one of the most active clubs in whole SRM!! </p>
-                        <img src={f2} alt="" className='w-[70px] absolute ml-[375px] -mt-7 ' />
-                    </div>
-                    <div className='flex gap-4 pt-20 pl-24 pb-10'>
-                        <div className='border-2 rounded-full p-3 hover:border-cyan-300 flex items-center justify-center transition-colors duration-200'>
-                            <GrInstagram size={20} className='text-white hover:text-cyan-300' />
-                        </div>
-                        <div className='border-2 rounded-full p-3 hover:border-cyan-300 flex items-center justify-center transition-colors duration-200'>
-                            <FaTwitter size={20} className='text-white hover:text-cyan-300' />
-                        </div>
-                        <div className='border-2 rounded-full p-3 hover:border-cyan-300 flex items-center justify-center transition-colors duration-200'>
-                            <FaLinkedinIn size={20} className='text-white hover:text-cyan-300' />
-                        </div>
-                        <div className='border-2 rounded-full p-3 hover:border-cyan-300 flex items-center justify-center transition-colors duration-200'>
-                            <IoLogoWhatsapp size={20} className='text-white hover:text-cyan-300' />
-                        </div>
+        <hr />
+        <footer className="bg-black text-white py-10">
+            <div className=" mx-auto">
+                {/* Use flex for main layout */}
+                <div className="flex flex-col justify-center items-center gap-[10px] md:flex-row  md:justify-between mb-10">
+                    <div className=" md:text-left mb-4 md:mb-0">
+                        <img src={ftr} alt="" className='w-[470px] px-4'/>
                     </div>
 
-
+                    {/* Combine all Quick Links into a single flex container */}
+                    <div className="flex flex-col md:flex-row md:justify-around mt-4 pr-8 md:mt-0 ">
+                        <div className="text-center md:text-left mb-4 md:mb-0">
+                            
+                            <ul className="text-gray-400 space-y-1 ">
+                                <li>
+                                    <a href="https://takeuforward.org/about-us" className="hover:text-cyan-400 transition duration-200 hover:underline">SRMIST KTR</a>
+                                </li>
+                                <li>
+                                    <a href="https://takeuforward.org/contact-us" className="hover:text-cyan-400 transition duration-200 hover:underline">codenex.club@srmist.com</a>
+                                </li>
+                                <li>
+                                    <a href="https://takeuforward.org/plus" className="hover:text-cyan-400 transition duration-200 hover:underline">+91-0000000000</a>
+                                </li>
+                               
+                            </ul>
+                        </div>
+                        {/* Repeat Quick Links for the other sections */}
+                       
+                        
+                        
+                    </div>
                 </div>
 
-                <div className=' pt-28 '>
-                    <div >
-                        <p className='text-white ml-[950px]'>SRMIST KTR</p>
-                        <p className='text-white mt-3 ml-[855px]'>codenex.club@srmist.com</p>
-                        <p className='text-white mt-3 ml-[918px]'>+91-0000000000</p>
-                    </div>
+                <div className="flex 
+    half-screen-padding         
+    lg:px-[70px]           
+    mb-6">
+    <a href="#" className="mx-3 text-white hover:text-cyan-400 transition duration-200">
+        <FaInstagram />
+    </a>
+    <a href="#" className="mx-3 text-white hover:text-cyan-400 transition duration-200">
+        <FaTwitter />
+    </a>
+    <a href="#" className="mx-3 text-white hover:text-cyan-400 transition duration-200">
+        <FaLinkedinIn />
+    </a>
+    <a href="#" className="mx-3 text-white hover:text-cyan-400 transition duration-200">
+        <IoLogoWhatsapp />
+    </a>
+</div>
 
+
+                <div className="border-t border-gray-700  pt-6 text-center">
+                    <p className="text-gray-400">
+                         Made by Sameer.
+                    </p>
                 </div>
-            </footer>
+            </div>
+        </footer>
         </>
-    )
-}
+    );
+};
 
-export default Footer
+export default Footer;
