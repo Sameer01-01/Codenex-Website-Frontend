4c6f9e1f6fc32b03d14f00bdff65e91a6f3f1e34
Sameer Yadav <ysameer0026@gmail.com>
2024-10-16 02:35:31 +0530

    navbar updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
 
 const Navbar = () => {
@@ -13,7 +14,7 @@ const Navbar = () => {
   return (
     <>
       {/* Navbar Container */}
-      <div className='bg-black text-white font-semibold flex items-center justify-between px-6 py-4'>
+      <div className='bg-black text-white font-semibold flex items-center justify-between px-6 py-4 sticky top-0 z-50'>
         {/* Logo */}
         <div className='flex items-center'>
           <img src={logo} alt="Logo" className='w-12' />
